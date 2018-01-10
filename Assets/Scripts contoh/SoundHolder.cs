9441cfc06c80f8e227ac947a12a6ad6c0fed9a81
AzmanFatahillah <azman.fatahillah1@gmail.com>
2018-01-10 18:11:48 +0700

    Grafkom

---
+++ Assets/Scripts contoh/SoundHolder.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class SoundHolder : MonoBehaviour {
+	//make a container for the heads up display
+	//pancam hud;
+	
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.tag == "Player") {
+			//hud = GameObject.Find ("Main Camera").GetComponent<pancam>();
+			//hud.increaseScore(10);
+			audio.Play();
+
+
+			//Destroy (gameObject);
+			Destroy (this.gameObject,1);
+			
+		}
+		
+	}
+}
